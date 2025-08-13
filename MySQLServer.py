beedac3507ebab8b0219e554362fa4eebebd8efc
BaldDev699 <methusellanyongesa004@gmail.com>
2025-08-13 05:50:03 +0300

    python script to create a MySQL database

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,7 +16,7 @@ try:
         cursor = connection.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error: Could not connect or execute query: {e}")
 finally:
     if cursor in locals():
